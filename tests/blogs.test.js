876b2c88e668bdf6934723fbfc2af7a78bc5c2e4
Ben Halverson <benhalverson33@gmail.com>
2019-10-24 01:40:25 -0700

    refactor(tests): more tests for blog post

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,18 +1,32 @@
 /* eslint-disable no-undef */
 const Page = require('./helpers/page');
 
-let page;
-beforeEach(async () => {
-  page = await Page.build();
-  await page.goto('localhost:3000');
+describe('When logged in', async () => {
+  let page;
+
+  beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('localhost:3000');
+    await page.login();
+    await page.click('a.btn-floating');
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  test('when signed in, create a new blog post', async () => {
+    const label = await page.getContentsOf('form label');
+    expect(label).toEqual('Blog Title');
+  });
+
+  xtest('when signed in, enter valid inputs into form', async () => {
+    await page.login();
+  });
+
+  xtest('when signed in, enter invalid inputs into form', async () => {});
 });
 
-afterEach(async () => {
-  await page.close();
-});
-test('when signed in, create a new blog post', async () => {
-  await page.login();
-  await page.click('a.btn-floating');
-  const label = await page.getContentsOf('form label');
-  expect(label).toEqual('Blog Title');
-});
+xtest('when NOT signed in, creating a post results in an error', async () => {});
+
+xtest('when NOT signed in, viewing a post results in an error', async () => {});
